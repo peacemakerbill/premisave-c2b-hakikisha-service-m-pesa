e975f58e3df01cf8bee5b9115dc8211c02cfa947
Bill Graham Peacemaker <billgrahamp@gmail.com>
2026-09-20 20:33:54 +0300

    creatined new c2b hakikisha project

---
+++ src/main/java/com/premisave/c2b_hakikisha/PremisaveC2bHakikishaServiceMPesaApplication.java
@@ -0,0 +1,13 @@
+package com.premisave.c2b_hakikisha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremisaveC2bHakikishaServiceMPesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremisaveC2bHakikishaServiceMPesaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/premisave/c2b_hakikisha/PremisaveC2bHakikishaServiceMPesaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premisave.c2b_hakikisha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremisaveC2bHakikishaServiceMPesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
